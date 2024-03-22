3e4a29beb71924fd8f4c16b0ba82a42f8d140cf0
Yug Shah <yugscontact@gmail.com>
2024-03-22 15:08:47 -0400

    Updated column layout for chart and total number

--- app/src/views/group2.js
+++ app/src/views/group2.js
@@ -10,8 +10,16 @@ const Group2 = (props) => {
       <Helmet>
         <title>Robarts Library Heat Map</title>
       </Helmet>
+      <div className="group2-frame31">
+        <div className="group2-text30">Robarts Library Heat Map</div>
+        <div className="images-row">
+          <img alt="image14051" src="/external/image14051-2nai-300h.png" className="group2-image1"/>
+          <img alt="image24051" src="/external/image24051-7on-300w.png" className="group2-image2"/>
+          <img alt="image34051" src="/external/image34051-brnq-300w.png" className="group2-image3"/>
+        </div>
+      </div>
       <div className="group2-group2">
-        <div className="group2-group1">
+        <div className="subscribe-box">
           <div className="group2-cta-section">
             <div className="group2-type-lock-up">
               <div className="group2-frame436">
@@ -51,39 +59,25 @@ const Group2 = (props) => {
               <div>
                   <input className="group2-text62 componentsbuttondefault" type="button" value="Sign me up!"/> 
               </div>
-
-              
             </div>
           </div>
         </div>
-        <div className="group2-login-page1">
-          <div className="group2-frame31">
-            <span className="group2-text30">
-              <span>Robarts Library Heat Map</span>
-            </span>
-            <div className="row">
-              <div className="column">
-                <img alt="image24051" src="/external/image24051-7on-300w.png" className="group2-image2"/>
-              </div>
-              <div className="column">
-                <img alt="image14051" src="/external/image14051-2nai-300h.png" className="group2-image1"/>
-              </div>
-              <div className="column">
-                <img alt="image34051" src="/external/image34051-brnq-300w.png" className="group2-image3"/>
-              </div>
+        <div className='group2-frame32'>
+          <div className="group2-text34">
+            <div className='row'>
+              <div class='column left'>1st Floor</div><div id="floor1" class='column middle'></div><div class='column right'></div>
             </div>
-
-            <div className="group2-text34">
-              <div>1st Floor</div>
-              <br></br>
-              <div>2nd Floor</div>
-              <br></br>
-              <div>3rd Floor</div>
-              <br></br>
-              <div>4th Floor</div>
-              <br></br>
-              <div>5th Floor</div>
-              <br></br>
+            <div className='row'>
+              <div class='column left'>2nd Floor</div><div id="floor2" class='column middle'></div><div class='column right'></div>
+            </div>
+            <div className='row'>
+              <div class='column left'>3rd Floor</div><div id="floor3" class='column middle'></div><div class='column right'></div>
+            </div>
+            <div className='row'>
+              <div class='column left'>4th Floor</div><div id="floor4" class='column middle'></div><div class='column right'></div>
+            </div>
+            <div className='row'>
+              <div class='column left'>5th Floor</div><div id="floor5" class='column middle'></div><div class='column right'></div>
             </div>
           </div>
         </div>
